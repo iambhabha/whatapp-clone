035229aa9d7874e7c1770e96cd13075fe7e9cd39
kapil sharma <kapilsharma@gtsinfosoft.com>
2022-12-29 21:45:26 +0530

    sdfsdf

---
+++ lib/feature/call/screens/call_screen.dart
@@ -0,0 +1,81 @@
+import 'package:agora_uikit/agora_uikit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:whatsapp_clone/models/call.dart';
+
+import '../../../common/utility/circularloader.dart';
+import '../controller/call_controller.dart';
+
+class CallScreen extends ConsumerStatefulWidget {
+  final String channelId;
+  final Call call;
+  final bool isGroupChat;
+  const CallScreen({
+    Key? key,
+    required this.channelId,
+    required this.call,
+    required this.isGroupChat,
+  }) : super(key: key);
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _CallScreenState();
+}
+
+class _CallScreenState extends ConsumerState<CallScreen> {
+  AgoraClient? client;
+  // String baseUrl = c.baseUrl;
+
+  @override
+  void initState() {
+    super.initState();
+    // client = AgoraClient(
+    //   agoraConnectionData: AgoraConnectionData(
+    //     appId: AgoraConfig.appId,
+    //     channelName: widget.channelId,
+    //     tokenUrl: baseUrl,
+    //   ),
+    // );
+    initAgora();
+  }
+
+  void initAgora() async {
+    await client!.initialize();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: client == null
+          ? const Loader()
+          : SafeArea(
+              child: Stack(
+                children: [
+                  AgoraVideoViewer(client: client!),
+                  AgoraVideoButtons(
+                    client: client!,
+                    disconnectButtonChild: IconButton(
+                      onPressed: () async {
+                        await client!.engine.leaveChannel();
+                        // ignore: use_build_context_synchronously
+                        ref.read(callControllerProvider).endCall(
+                              widget.call.callerId,
+                              widget.call.receiverId,
+                              context,
+                            );
+                        ref.read(callControllerProvider).endGroupCall(
+                            widget.call.callerId,
+                            widget.call.receiverId,
+                            context);
+                        // ignore: use_build_context_synchronously
+                        Navigator.pop(context);
+                      },
+                      icon: const Icon(Icons.call_end),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:whatsapp_clone/firebase_options.dart';
+
+import 'common/utility/circularloader.dart';
+import 'common/utility/error.dart';
+import 'feature/auth/controller/auth_controller.dart';
+import 'feature/landing/screens/landing_screen.dart';
+import 'mobile_chat_screen.dart';
+import 'router.dart';
+import 'common/utility/colors.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends ConsumerWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return MaterialApp(
+      title: 'Whatsapp Clone',
+      theme: ThemeData.dark().copyWith(
+          backgroundColor: backgroundColor,
+          appBarTheme: const AppBarTheme(color: appBarColor)),
+      onGenerateRoute: (settings) => generateRoute(settings),
+      home: ref.watch(userDataAuthProvider).when(
+            data: (user) {
+              if (user == null) {
+                return const WelcomePage();
+              }
+              return const MobileLayoutScreen();
+            },
+            error: (err, trace) {
+              return ErrorScreen(
+                error: err.toString(),
+              );
+            },
+            loading: () => const Loader(),
+          ),
+    );
+  }
+}
